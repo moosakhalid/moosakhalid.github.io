b0acde9d56a5e2326992a60020987bdf1419460d
Moosa Khalid <moosakhalid@gmail.com>
2020-05-29 01:36:43 -0400

    Created my_react.js
    Added React Code

---
+++ src/my_react.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+Banner.defaultProps = { banner: 'Site under construction: moosakhalid.com' }
+
+Banner.propTypes = { banner: PropTypes.string.isRequired };
+
+class Banner extends React.Component {
+   constructor(props) {
+      super(props);
+   }
+   render() {
+      return (
+      <div>
+      <h1>{props.banner}</h1>
+      </div>
+      );
+   }
+};
+
+ReactDOM.render(<Banner />, document.getElementById('container');
