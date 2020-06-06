ae49bc6435e34945cbf21722e8db04bbb55495f4
Moosa Khalid <moosa.khalid@linuxacademy.com>
2020-06-06 02:47:42 -0400

    Updates

---
+++ precache-manifest.1c6a21b22bb6c7789fe79e9e2096c86c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7a1a9acb8b95f31dff46b20d43809d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a09c9ba9601ed6e4e735",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3b3b7e629bf9bd4d3c8",
+    "url": "/static/js/2.df5d811d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.df5d811d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a09c9ba9601ed6e4e735",
+    "url": "/static/js/main.6e7102a5.chunk.js"
+  },
+  {
+    "revision": "c815442ab336b747f527",
+    "url": "/static/js/runtime-main.94a6af97.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
